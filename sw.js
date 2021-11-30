ec1c4ff91bcc165b560c901dfefc4f5f462c9427
NeerajTulsani <47379811+NeerajTulsani@users.noreply.github.com>
2021-11-30 14:26:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 self.addEventListener('push', e => {
     const data = e.data.json();
+    console.log(data);
     self.registration.showNotification(data.data, {
       body: 'sent by neeraj tulsani' 
     });
